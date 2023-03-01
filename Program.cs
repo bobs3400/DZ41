7dc16c36a17dec27b4b78046836716390cd8d6ab
bobs3400 <124318533+bobs3400@users.noreply.github.com>
2023-03-01 20:23:34 +1000

    41

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Clear();
+Console.WriteLine("Введите элементы массива через пробел: ");
+string elements = Console.ReadLine();
+int[] baseArray = GetArrayFromString(elements);
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ");
+    int[] res = new int[nums.Length];
+
+    for(int i = 0; i < nums.Length; i++)
+    {
+        res[i] = int.Parse(nums[i]);
+    }
+    return res;
+}
+int KolNum(int[] mass)
+{   
+    int n = 0;
+    for(int i = 0; i < mass.Length; i++)
+    {
+        if(mass[i] > 0)
+        n++;
+    }
+    return n;
+}
+if(KolNum(baseArray) > 0)
+{
+    Console.WriteLine($"В данном массиве {KolNum(baseArray)} чисел больше 0");
+}
+else
+{
+    Console.WriteLine($"В данном массиве нет чисел больше 0");
+}
